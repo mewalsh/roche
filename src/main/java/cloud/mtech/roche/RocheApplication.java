cd5439062949368e744acf638e3014d3243d3ffc
martinw <martin.walsh@outlook.com>
2020-04-15 19:16:30 +0100

    Project Skeleton

---
+++ src/main/java/cloud/mtech/roche/RocheApplication.java
@@ -0,0 +1,13 @@
+package cloud.mtech.roche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/mtech/roche/RocheApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.mtech.roche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
